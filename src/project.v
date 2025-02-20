6f8704eddb84c00ec9aebbc77aa830a31e33ea2a
suba5528 <subashini.2022105528@gmail.com>
2025-02-20 23:33:43 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module SPI2 (
+module tt_um_suba (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
