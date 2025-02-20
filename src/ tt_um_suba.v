de817a9b0ee398d610383737cdd61d68f78b86ed
suba5528 <subashini.2022105528@gmail.com>
2025-02-21 00:29:47 +0530

    Update  tt_um_suba.v

--- src/ tt_um_suba.v
+++ src/ tt_um_suba.v
@@ -2,8 +2,9 @@
  * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
-
 `default_nettype none
+
+`timescale 1ns / 1ps
 
 module tt_um_suba (
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -15,96 +16,83 @@ module tt_um_suba (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-    reg [7:0] data = 8'b10101001;          // Data to send via MISO
-    reg [7:0] received_data;              // Data received from MOSI
-    reg [2:0] state_mosi;                 // State for MOSI FSM
-    reg [2:0] state_miso;                 // State for MISO FSM
-    reg [7:0] read_data;                    // Data to send via MISO
-
-    // State definitions
-    localparam MOSI_IDLE_STATE  = 0;
-    localparam MOSI_START_STATE = 1;
-    localparam READ_STATE       = 2;
-    localparam MOSI_STOP_STATE  = 3;
-    localparam MISO_IDLE_STATE  = 4;
-    localparam MISO_START_STATE = 5;
-    localparam WRITE_STATE      = 6;
-    localparam MISO_STOP_STATE  = 7;
-
+    // Internal registers
+    reg [7:0] received_data;  // Data received from MOSI
+    reg [7:0] transmit_data = 8'b10101001; // Data to send via MISO
+    reg [2:0] state_mosi;     // MOSI state machine
+    reg [2:0] state_miso;     // MISO state machine
     reg [3:0] mosi_counter;
     reg [3:0] miso_counter;
-    wire spi_clk;
     reg miso_out;
+
+    // Assignments
+    wire cs   = ui_in[0]; // Chip Select
+    wire mosi = ui_in[1]; // MOSI input
+    wire spi_clk = clk;
     
-    assign spi_clk = clk;
-    assign uo_out  = 0;
+    assign uo_out  = {7'b0, miso_out};
     assign uio_out = 0;
     assign uio_oe  = 0;
     
-    initial begin
-        state_mosi = MOSI_IDLE_STATE;
-        state_miso = MISO_IDLE_STATE;
-        miso_out = 1;
-    end
-
-    // MOSI state machine (data reception from master)
+    // MOSI State Machine (Receiving Data)
     always @(negedge spi_clk) begin
         if (!rst_n) begin
-            received_data = 0;
-            state_mosi = MOSI_IDLE_STATE;
-            mosi_counter = 7;
+            received_data <= 0;
+            state_mosi <= 0;
+            mosi_counter <= 7;
         end else begin
             case(state_mosi)
-                MOSI_IDLE_STATE: begin
-                    if (ui_in[0] == 0) state_mosi = MOSI_START_STATE;
-                end
-                MOSI_START_STATE: begin
-                    if (ui_in[1] == 0) begin // Start bit detected
-                        state_mosi = READ_STATE;
-                        mosi_counter = 7;
-                    end
-                end
-                READ_STATE: begin
-                    received_data[mosi_counter] = ui_in[2]; // Store bit from MOSI
-                    if (mosi_counter == 0) state_mosi = MOSI_STOP_STATE;
-                    mosi_counter = mosi_counter - 1;
-                end
-                MOSI_STOP_STATE: state_mosi = MOSI_IDLE_STATE;
+                0: if (!cs) state_mosi <= 1;
+                1: if (!cs) begin
+                        state_mosi <= 2;
+                        mosi_counter <= 7;
+                   end
+                2: begin
+                    received_data[mosi_counter] <= mosi;
+                    if (mosi_counter == 0) state_mosi <= 3;
+                    mosi_counter <= mosi_counter - 1;
+                   end
+                3: state_mosi <= 0;
             endcase
         end
     end
 
-    // MISO state machine (data transmission to master)
+    // MISO State Machine (Transmitting Data)
     always @(posedge spi_clk) begin
         if (!rst_n) begin
-            read_data = 0;
-            state_miso = MISO_IDLE_STATE;
-            miso_counter = 7;
+            state_miso <= 0;
+            miso_counter <= 7;
+            miso_out <= 1;
         end else begin
             case(state_miso)
-                MISO_IDLE_STATE: begin
-                    if (ui_in[0] == 0) state_miso = MISO_START_STATE;
-                end
-                MISO_START_STATE: begin
-                    miso_out = 0; // Start bit
-                    state_miso = WRITE_STATE;
-                    read_data = received_data;
-                    miso_counter = 7;
-                end
-                WRITE_STATE: begin
-                    miso_out = read_data[miso_counter]; // Send bit to MISO
-                    if (miso_counter == 0) state_miso = MISO_STOP_STATE;
-                    miso_counter = miso_counter - 1;
-                end
-                MISO_STOP_STATE: begin
-                    miso_out = 1; // Stop bit
-                    state_miso = MISO_IDLE_STATE;
-                end
+                0: if (!cs) state_miso <= 1;
+                1: begin
+                    miso_out <= 0;
+                    state_miso <= 2;
+                    miso_counter <= 7;
+                   end
+                2: begin
+                    miso_out <= transmit_data[miso_counter];
+                    if (miso_counter == 0) state_miso <= 3;
+                    miso_counter <= miso_counter - 1;
+                   end
+                3: begin
+                    miso_out <= 1;
+                    state_miso <= 0;
+                   end
             endcase
         end
     end
 
     // Unused input assignments to avoid warnings
-    wire _unused = &{ena, 1'b0};
+    wire _unused = &{ena, ui_in[2], ui_in[3], ui_in[4], ui_in[5], ui_in[6], ui_in[7], uio_in[7:0]};
 
 endmodule
+
+
+
+
+   
+                 
+
+   
